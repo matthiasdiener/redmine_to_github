ab90af92eec3001ec44a97ae9060b339a29f2dfb
Matthias Diener <mdiener@illinois.edu>
2019-03-17 19:06:50 -0500

    factor out request creation

--- upload_github.py
+++ upload_github.py
@@ -10,43 +10,47 @@ import time
 from config import *
 
 
-def get_translate_dict():
-    d = {}
-    for hlevel in range(1, 7):
-        d['h%s.' % hlevel] = '#' * hlevel        # e.g. d['h2.'] = '##'
-    d['\n# '] = '\n1. '  # lists
-    d['<pre>'] = '```'  # code block
-    d['</pre>'] = '\n```'  # code block
-    d['@'] = '`'  # inline code
-    return d
-
 def translate_for_github(content):
     if not content:
         return None
-    
-    for k, v in get_translate_dict().items():
-        content = content.replace(k, v)
+
+    for hlvl in range(1, 7):
+        content = content.replace('h{0}.'.format(hlvl), '#' * hlvl)
+
+    content = content.replace('@', '`')        # inline code
+    content = content.replace('\n# ', '1. ')   # lists
+    content = content.replace('\r\n<pre>\r\n', '\n```\n')  # code block
+    content = content.replace('<pre>', '\n```\n')  # code block
+    content = content.replace('\r\n</pre>\r\n', '\n```\n') # code block
+    content = content.replace('</pre>', '\n```\n') # code block
 
     return content
 
-# Map from milestone name to Github milestone ID
+
+def make_request(_url, _user, _data=None, _type='GET', _headers={}):
+    _headers['Authorization'] = 'token {0}'.format(get_github_token(_user))
+
+    url = 'https://api.github.com/repos/{0}'.format(GITHUB_REPO) + _url
+
+    time.sleep(2)
+    response = requests.request(_type, url, data=_data, headers=_headers)
+
+    return response
+
+# Map from Redmine milestone name to Github milestone ID
 milestones = {}
 
 def get_milestones(user):
     if milestones:
         return 
 
-    url = 'https://api.github.com/repos/{0}/milestones'.format(GITHUB_REPO)
-    headers = {
-    "Authorization": "token {0}".format(github_tokenmap[user])
-    }
-
-    response = requests.request("GET", url, headers=headers)
+    response = make_request('/milestones', user)
 
     j = json.loads(response.content)
 
     for n in j:
         milestones[n["title"]] = n["number"]
+
 
 def create_milestone(user, title):
 
@@ -58,18 +62,11 @@ def create_milestone(user, title):
     if title in milestones:
         return milestones[title]
 
-    url = 'https://api.github.com/repos/{0}/milestones'.format(GITHUB_REPO)
-
-    headers = {
-    "Authorization": "token {0}".format(github_tokenmap[user])
-    }
-
     data = {"title": title}
 
     payload = json.dumps(data)
 
-    time.sleep(2)
-    response = requests.request("POST", url, data=payload, headers=headers)
+    response = make_request('/milestones', user, _data=payload, _type="POST")
 
     j = json.loads(response.content)
 
@@ -78,25 +75,22 @@ def create_milestone(user, title):
     return milestones[title]
 
 
-
 def make_issue(user, title, body, created_at, closed_at, updated_at, assignee, milestone, closed, labels):
     # Create an issue on github.com using the given parameters
 
-    realuser = user
+    realuser = get_github_username(user)
 
     if body == None:
         body = "No body."
 
-    if realuser not in github_tokenmap:
-        realuser = github_default_username
+    if not redmine_user_has_token(user):
         body = "*Original author: " + user + "*\n\n---\n" + body
 
     if assignee not in github_tokenmap:
         assignee = github_default_username
 
     headers = {
-    "Authorization": "token {0}".format(github_tokenmap[realuser])
-    "Accept": "application/vnd.github.golden-comet-preview+json"
+        "Accept": "application/vnd.github.golden-comet-preview+json"
     }
 
     url = 'https://api.github.com/repos/{0}/import/issues'.format(GITHUB_REPO)
@@ -106,7 +100,7 @@ def make_issue(user, title, body, created_at, closed_at, updated_at, assignee, m
                       'created_at': created_at,
                       'updated_at': updated_at,
                       'assignee': assignee,
-                      'milestone': create_milestone(realuser, milestone),
+                      'milestone': create_milestone(github_default_username, milestone),
                       'closed': closed,
                       'labels': labels
                       }}
@@ -115,11 +109,7 @@ def make_issue(user, title, body, created_at, closed_at, updated_at, assignee, m
 
     payload = json.dumps(data)
 
-    # Add the issue to the repository
-
-    # GitHub has a limit of 300 requests/per hour
-    time.sleep(2)
-    response = requests.request("POST", url, data=payload, headers=headers)
+    response = make_request('/import/issues', realuser, _data=payload, _type="POST", _headers=headers)
     if response.status_code != 202:
         print('Could not create issue "{0}"'.format(title))
         print('Response:', response.content)
@@ -136,7 +126,7 @@ def make_issue(user, title, body, created_at, closed_at, updated_at, assignee, m
                 issue_url = r["issue_url"]
                 num = int(issue_url[issue_url.rfind("/")+1:])
                 real_url = issue_url.replace("api.github.com/repos/", "github.com/")
-                print ('Created issue #%d "%s" %s' % (num, title, real_url))
+                print('Created issue #{0} "{1}" {2}'.format(num, title[:50], real_url))
                 break
 
             if status == "failed":
@@ -153,25 +143,19 @@ def make_comment(user, issuenr, body, ctime):
     orig_body = body
     body = "*Original date: " + ctime + "*\n\n---\n" + body
 
-    realuser = user
+    realuser = get_github_username(user)
 
-    if realuser not in github_tokenmap:
-        realuser = github_default_username
+    if not redmine_user_has_token(user):
         body = "*Original author: " + user + "*\n" + body
 
-    headers = {
-        "Authorization": "token {0}".format(github_tokenmap[realuser])
-    }
 
-
-    url = 'https://api.github.com/repos/{0}/issues/{1}/comments'.format(GITHUB_REPO, issuenr)
+    url = '/issues/{0}/comments'.format(issuenr)
 
     data = {'body' : body}
 
     payload = json.dumps(data)
 
-    time.sleep(2)
-    response = requests.request("POST", url, data=payload, headers=headers)
+    response = make_request(url, realuser, _type="POST", _data=payload)
 
     if response.status_code != 201:
         print('Could not create comment: "{0}"'.format(body))
@@ -179,19 +163,33 @@ def make_comment(user, issuenr, body, ctime):
         sys.exit(1)
     else:
         if "X-RateLimit-Remaining" in response.headers: 
-            print('  Created comment: {0} [...] ({1})'.format(orig_body.split('\n')[0], response.headers["X-RateLimit-Remaining"]))
+            print('  Created comment: "{0}" ({1})'.format(orig_body.split('\n')[0][:50], response.headers["X-RateLimit-Remaining"]))
         else:
-            print('  Created comment: {0} [...] ({1})'.format(orig_body.split('\n')[0]), "unknown")
+            print('  Created comment: "{0}"'.format(orig_body.split('\n')[0][:50]))
 
+
+def redmine_user_has_token(redmine_user):
+    if redmine_user in github_usermap:
+        github_user = github_usermap[redmine_user]
+        if github_user in github_tokenmap:
+            return True
+    return False
 
 
 def get_github_username(redmine_user):
     if redmine_user in github_usermap:
         return(github_usermap[redmine_user])
     else:
-        # print('  Redmine user "{0}" not in github_usermap, using default github author "{1}".'.format(redmine_user, github_default_username))
+        print('  Redmine user "{0}" not in github_usermap, using default GitHub user "{1}".'.format(redmine_user, github_default_username))
         return(github_default_username)
 
+
+def get_github_token(github_user):
+    if github_user in github_tokenmap:
+        return(github_tokenmap[github_user])
+    else:
+        print('  GitHub user "{0}" not in github_tokenmap, using token for default GitHub user "{1}".'.format(github_user, github_default_username))
+        return(github_tokenmap[github_default_username])
 
 
 def create_issue_from_redmine_file(filename):
@@ -199,9 +197,7 @@ def create_issue_from_redmine_file(filename):
     with open(filename) as infile:
         indata = json.load(infile)
 
-    redmine_user = indata["author"]["name"]
-
-    github_author = get_github_username(redmine_user)
+    author = indata["author"]["name"]
 
     filename_issue_num = int(os.path.basename(filename).split('.')[0])
 
@@ -238,14 +234,14 @@ def create_issue_from_redmine_file(filename):
 
 
     # Create issue
-    github_issue_num = make_issue(github_author, title, body, created_at, closed_at, updated_at, assignee, milestone, closed, labels)
+    github_issue_num = make_issue(author, title, body, created_at, closed_at, updated_at, assignee, milestone, closed, labels)
 
     if github_issue_num != filename_issue_num:
-        print('  Warning: GitHub issue number ({0}) does not match Redmine issue number ({1})!'.format(github_issue_num, filename_issue_num))
+        print('  ** Warning: GitHub issue number ({0}) does not match Redmine issue number ({1})! **'.format(github_issue_num, filename_issue_num))
 
     # Add comments
     for j in indata["journals"]:
-        github_author = get_github_username(j["user"]["name"])
+        author = j["user"]["name"]
 
         body = None
 
@@ -258,8 +254,15 @@ def create_issue_from_redmine_file(filename):
 
         if body == "" or body == None: continue
 
-        make_comment(github_author, github_issue_num, body, created_at)
+        make_comment(author, github_issue_num, body, created_at)
 
 
-for f in sys.argv[1:]:
+files = sys.argv[1:]
+
+print('='*80)
+print('Uploading {0} issue(s) to GitHub repository "{1}".'.format(len(files), GITHUB_REPO))
+print('='*80)
+
+
+for f in files:
     create_issue_from_redmine_file(f)
